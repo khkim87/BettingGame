ad6878d4bf5862a1c891faa927bbd293c723e6bf
khkim <khkim@en-core.com>
2020-02-06 13:18:34 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
-const userText = document.querySelector('.userList');
+const userText = document.querySelector('.userText');
 const setBtn = document.querySelector('.userSetBtn');
 const startBtn = document.querySelector('.startBtn');
 const rulret = document.querySelector('.rulret');
+const joinList = document.querySelector('.joinList');
 
 function canvers(list){
     var ctx = document.querySelector('.cvs').getContext("2d");
     let radius = 120;
 
-    ctx.beginPath();
+    
     ctx.clearRect(0,0,300,300);
     //그리기
     list.map(user =>{
+        ctx.beginPath();
         ctx.moveTo(150,150);
         ctx.arc(150,150, radius, (Math.PI/180) * user.startPoint, (Math.PI/180) * user.endPoint ,false);
-        // ctx.fillStyle = 'orange';
-        // ctx.fill();
+        ctx.fillStyle = user.color;
+        ctx.fill();
         ctx.stroke();
     });
     ctx.closePath();
@@ -25,15 +27,23 @@ function userSetting(userText){
     let userList = userText.split(',');
     let currPoint = 270;
 
+    joinList.childNodes.forEach(node =>{
+        joinList.removeChild(node);
+    })
     userList.map(user => {
         let endPoint = currPoint + (360/userList.length);
         endPoint = endPoint < 360 ? endPoint : endPoint - 360;
         list.push({
             userName: user,
+            color: '#'+Math.round(Math.random()* 0xffffff).toString(16),
             startPoint: currPoint,
             endPoint: endPoint
         });
         currPoint = endPoint;
+        
+        var node = document.createElement('li'); 
+        node.innerText = user
+        joinList.appendChild(node);
     });
 
     console.log(userList);
@@ -46,6 +56,11 @@ function clickSetButton(){
 
 function start(){
     rulret.classList.toggle('start');
+    let max = 9000;
+    let min = 5000;
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(randomNum);
+    rulret.style.transform = `rotate(${randomNum}deg)`;
 }
 
 function init(){
